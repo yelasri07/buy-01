3971c16fc86b4b77ada7f3e0c721f5c8307091f8
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2026-04-28 13:21:32 +0100

    added health check / condition for discovery to make sure its runing before others

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 cd user-service
-mvn clean package
+./mvnw clean package
 cd ..
 cd discovery-service
-mvn clean package
+./mvnw clean package
 docker-compose down
 docker rmi buy-01-discovery-service:latest
 docker rmi buy-01-user-service:latest
