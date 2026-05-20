e03dc8ac83f7fc50e92a1b376eb2bd5a0eedb68f
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2026-05-20 18:03:48 +0100

    save

--- frontend/src/app/core/services/product.service.ts
+++ frontend/src/app/core/services/product.service.ts
@@ -2,15 +2,19 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable, signal, computed } from '@angular/core';
 import { Product } from '../interfaces/product.interface';
 import { API } from '../config/api';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   private http = inject(HttpClient);
-  
+
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+
   private readonly pageSize = 6;
-  private readonly _page = signal(0);
+  private readonly _page = signal(1);
   private readonly _products = signal<Product[]>([]);
   private readonly _loading = signal(false);
 
@@ -18,43 +22,56 @@ export class ProductService {
   readonly page = computed(() => this._page());
   readonly products = computed(() => this._products());
   readonly loading = computed(() => this._loading());
-  readonly isFirstPage = computed(() => this._page() === 0);
+  readonly isFirstPage = computed(() => this._page() === 1);
   readonly isLastPage = computed(() => this._products().length < this.pageSize);
 
   constructor() {
+    this.route.queryParams.subscribe((params) => {
+      this._page.set(+params['page'] || 1);
+    });
     this.loadProducts();
   }
 
   loadProducts() {
     this._loading.set(true);
     const params = new HttpParams()
-      .set('page', this._page().toString())
+      .set('page', (this._page() - 1).toString())
       .set('size', this.pageSize.toString());
 
     this.http.get<Product[]>(API.GET_POSTS, { params }).subscribe({
       next: (products) => {
-        console.log(products)
+        console.log(products);
         this._products.set(products);
         this._loading.set(false);
       },
       error: (err) => {
         this._loading.set(false);
-        throw err
-      }
+        throw err;
+      },
     });
   }
 
   nextPage() {
     if (!this.isLastPage()) {
-      this._page.update(p => p + 1);
+      this._page.update((p) => p + 1);
+      this.onPageChange(this._page());
       this.loadProducts();
     }
   }
 
   previousPage() {
     if (!this.isFirstPage()) {
-      this._page.update(p => Math.max(0, p - 1));
+      this._page.update((p) => Math.max(1, p - 1));
+      this.onPageChange(this._page());
       this.loadProducts();
     }
+  }
+
+  onPageChange(newPage: number) {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { page: newPage },
+      queryParamsHandling: 'merge', // preserves other params
+    });
   }
 }
--- frontend/src/app/features/home/components/product-card/product-card.component.ts
+++ frontend/src/app/features/home/components/product-card/product-card.component.ts
@@ -1,20 +1,23 @@
-import { Component, input, computed } from '@angular/core';
+import { Component, input, computed, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Product } from '../../../../core/interfaces/product.interface';
 import { CurrencyPipe } from '@angular/common';
+// import {  } from '@tailwindplus/elements'
+import '@tailwindplus/elements';
 
 @Component({
   selector: 'app-product-card',
   imports: [CurrencyPipe],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrl: './product-card.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ProductCardComponent {
   product = input.required<Product>();
 
   productImage = computed(() => {
     const files = this.product().files;
-    return (files && files.length > 0) 
-      ? files[0] 
+    return (files && files.length > 0)
+      ? files[0]
       : `https://placehold.co/600x400/222222/FFFFFF?text=${encodeURIComponent(this.product().name)}`;
   });
 
