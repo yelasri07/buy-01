d453edf4029dc691245706a0c2ef705747be32d1
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2026-06-28 11:38:47 +0100

    removing @SpringBootTest

--- media-service/src/test/java/media_service/MediaServiceApplicationTests.java
+++ media-service/src/test/java/media_service/MediaServiceApplicationTests.java
@@ -3,7 +3,7 @@ package media_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class MediaServiceApplicationTests {
 
 	@Test
